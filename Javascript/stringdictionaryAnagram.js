c6813b269fc81e4506339f97d16a48ed801402f8
Ronald Vimal Sebasdiyar <97762068+ronaldvimal@users.noreply.github.com>
2022-03-04 09:39:42 -0500

    Update with Java Solution as well

--- Javascript/stringdictionaryAnagram.js
+++ Javascript/stringdictionaryAnagram.js
@@ -24,3 +24,20 @@ const searchDictionary = (dictionary, searchWord) => {
 }
 
 console.log(searchDictionary(dictionary, searchWord));
+
+//Java Solution 
+
+// class Solution {
+//     public List<List<String>> searchDictionary(String[] strs) {
+//         if (strs.length == 0) return new ArrayList();
+//         Map<String, List> ans = new HashMap<String, List>();
+//         for (String s : strs) {
+//             char[] ca = s.toCharArray();
+//             Arrays.sort(ca);
+//             String key = String.valueOf(ca);
+//             if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+//             ans.get(key).add(s);
+//         }
+//         return new ArrayList(ans.values());
+//     }
+// }
